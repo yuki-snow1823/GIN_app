b917b788192515519db3f98da12db81b076427ec
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-11-23 07:41:13 +0900

    add model init DB

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/mattn/go-sqlite3" // インポートするけど使わない場合 _ を記述
 )
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 	data := "Hello Go/Gin!!"
-
 
 	router.GET("/", func(ctx *gin.Context) {
 		// 第3引数は渡したいデータかも
@@ -17,4 +19,22 @@ func main() {
 	})
 
 	router.Run()
+
+}
+
+// Model Setting
+type Todo struct {
+	gorm.Model
+	Text   string
+	Status string
+}
+
+// DB init setting
+func dbInit() {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("database can't open")
+	}
+	db.AutoMigrate(&Todo{})
+	defer db.Close()
 }
