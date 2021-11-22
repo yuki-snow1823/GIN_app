7bdec98572cd375c645a3d2fdfd798dc5fed2af0
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-11-23 07:34:13 +0900

    add: data transfer

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
+	data := "Hello Go/Gin!!"
+
 
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{})
+		// 第3引数は渡したいデータかも
+		// ctx.HTML(200, "index.html", gin.H{})
+		ctx.HTML(200, "index.html", gin.H{"data": data})
 	})
 
 	router.Run()
